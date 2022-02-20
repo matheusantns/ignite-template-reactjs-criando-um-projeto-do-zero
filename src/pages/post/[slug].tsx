6c53326e748c29ae0ffb69711d6be1502daf1786
Matheus Antunes <mta.santos@gmail.com>
2022-02-20 19:47:49 -0300

    version 1 finished

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -2,8 +2,7 @@ import { GetStaticPaths, GetStaticProps } from 'next'
 import Header from '../../components/Header'
 import Prismic from '@prismicio/client'
 import { useRouter } from 'next/router'
-import { PrismicRichText } from "@prismicio/react";
-import * as prismicH from '@prismicio/helpers';
+import { RichText } from 'prismic-dom';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -34,23 +33,22 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post(props: PostProps) {
+export default function Post({post}: PostProps) {
 
-  console.log(props.post.data.content)
+  console.log(post)
 
   const router = useRouter();
 
-  let words = [];
+  const totalWords = post.data.content.reduce((total, contentItem) => {
+    total += contentItem.heading.split(' ').length;
 
-  props.post.data.content.map(item => {
-    words.push(item.heading[0].text)
-    item.body.map(itemFinal => {
-      words.push(itemFinal.text)
-    })
-  })
+    const words = contentItem.body.map(item => item.text.split(' ').length);
+    words.map(word => (total += word));
+    return total;
+  }, 0);
 
-  const wordsCounted = words.join(" ").split(" ").length
-  const timeReading = Math.ceil(wordsCounted / 200);
+  const readTime = Math.ceil(totalWords / 200);
+
 
   if (router.isFallback) {
     return <div className=".loading">Carregando...</div>
@@ -60,27 +58,29 @@ export default function Post(props: PostProps) {
       <div className={styles.headerWrapper}>
         <Header />
       </div>
-        <img className={styles.bannerImg} src={props.post.data.banner.url} alt="banner" />
+        <img className={styles.bannerImg} src={post.data.banner.url} alt="banner" />
         <div className={commonStyles.container}>
           <div className={styles.headerTitle}>
-            <h1>{props.post.data.title}</h1>
+            <h1>{post.data.title}</h1>
             <div>
-              <span><FiCalendar /> {dateFormat(props.post.first_publication_date)}</span>
-              <span><FiUser /> {props.post.data.author}</span>
-              <span><FiClock /> {timeReading} min</span>
+              <span><FiCalendar /> {dateFormat(post.first_publication_date)}</span>
+              <span><FiUser /> {post.data.author}</span>
+              <span><FiClock /> {readTime} min</span>
             </div>
-            <article className={styles.post}>
-            {props.post.data.content.map((p, index) => {
+            </div>
+            {post.data.content.map((content, index) => {
               return (
-                  <article key={index} className={styles.postArticle}>
-                      <h3>{p.heading}</h3>
-                      <p dangerouslySetInnerHTML={{ __html: prismicH.asHTML(p.body) }} />
-                  </article>
-              )
-              }
-              )}
-        </article>
-          </div>
+                <article className={styles.post} key={index}>
+                  <h2>{content.heading}</h2>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: RichText.asHtml(content.body),
+                    }}
+                  />
+                </article>
+          );
+        })}
+
         </div>
     </>
   )
@@ -104,13 +104,32 @@ export const getStaticPaths: GetStaticPaths  = async () => {
   }
 };
 
-export const getStaticProps = async context => {
+export const getStaticProps: GetStaticProps = async context => {
   const prismic = getPrismicClient();
-  const response = await prismic.getByUID('posts', context.params.slug, {});
+  const response = await prismic.getByUID('posts', String(context.params.slug), {});
 
-  const post = {...response}
+  const post = {
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
+    last_publication_date: response.last_publication_date,
+    data: {
+      title: response.data.title,
+      subtitle: response.data.subtitle,
+      author: response.data.author,
+      banner: {
+        url: response.data.banner.url,
+      },
+      content: response.data.content.map(content => {
+        return {
+          heading: content.heading,
+          body: [...content.body],
+        };
+      }),
+    },
+  };
 
   return {
-    props: {post}
+    props: {post},
+    revalidate: 1 * 60 * 30 // 30 minutes
   }
 };
